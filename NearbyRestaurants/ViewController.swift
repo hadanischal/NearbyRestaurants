266a797d3cc10d7be01f755ac8f3c26acf7ac383
Nischal Hada <hadanischal@gmail.com>
2020-07-07 21:05:58 +1000

    Initial Commit

---
+++ NearbyRestaurants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearbyRestaurants
+//
+//  Created by Nischal Hada on 7/7/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
