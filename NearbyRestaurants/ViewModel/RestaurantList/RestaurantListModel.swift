38ce4f2e96197d8c27956407ba0d122d38f2cda2
Nischal Hada <hadanischal@gmail.com>
2020-07-12 15:50:40 +1000

    [Model] Added RestaurantListModel and RestaurantListModelTests

---
+++ NearbyRestaurants/ViewModel/RestaurantList/RestaurantListModel.swift
@@ -0,0 +1,33 @@
+//
+//  RestaurantListModel.swift
+//  NearbyRestaurants
+//
+//  Created by Nischal Hada on 10/7/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import Foundation
+
+struct RestaurantListModel: Equatable {
+    let id, name, url: String
+    let address: String
+    let imageUrl: String
+}
+
+extension RestaurantListModel {
+    init(_ data: RestaurantModel) {
+        id = data.id
+        name = data.name
+        url = data.url
+        address = "\(data.location.address) , \(data.location.city)"
+        imageUrl = data.photosUrl // featuredImage //photosUrl
+    }
+
+    init(_ data: Restaurant) {
+        id = "\(data.collectionId)"
+        name = data.title
+        url = data.url
+        address = data.description
+        imageUrl = data.imageUrl
+    }
+}
---
+++ NearbyRestaurantsTests/ViewModelTests/RestaurantListModelTests.swift
@@ -0,0 +1,54 @@
+//
+//  RestaurantListModelTests.swift
+//  NearbyRestaurantsTests
+//
+//  Created by Nischal Hada on 11/7/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import Cuckoo
+import Nimble
+import Quick
+import XCTest
+
+@testable import NearbyRestaurants
+
+final class RestaurantListModelTests: QuickSpec {
+    override func spec() {
+        describe("RestaurantListModel test") {
+            context("When set RestaurantListModel from RestaurantModel") {
+                let restaurantModel: RestaurantModel! = MockData().stubRestaurantResult()?.restaurants.first?.restaurant
+                var testModel: RestaurantListModel!
+
+                beforeEach {
+                    testModel = RestaurantListModel(restaurantModel)
+                }
+
+                it("it sets RestaurantListModel correctly") {
+                    expect(testModel.id).to(equal(restaurantModel.id))
+                    expect(testModel.name).to(equal(restaurantModel.name))
+                    expect(testModel.url).to(equal(restaurantModel.url))
+                    expect(testModel.address).to(equal("\(restaurantModel.location.address) , \(restaurantModel.location.city)"))
+                    expect(testModel.imageUrl).to(equal(restaurantModel.photosUrl))
+                }
+            }
+
+            context("When set RestaurantListModel from Restaurant") {
+                let restaurant: Restaurant! = MockData().stubCollectionsResult()?.collections.first?.restaurant
+                var testModel: RestaurantListModel!
+
+                beforeEach {
+                    testModel = RestaurantListModel(restaurant)
+                }
+
+                it("it sets RestaurantListModel correctly") {
+                    expect(testModel.id).to(equal("\(restaurant.collectionId)"))
+                    expect(testModel.name).to(equal(restaurant.title))
+                    expect(testModel.url).to(equal(restaurant.url))
+                    expect(testModel.address).to(equal(restaurant.description))
+                    expect(testModel.imageUrl).to(equal(restaurant.imageUrl))
+                }
+            }
+        }
+    }
+}
